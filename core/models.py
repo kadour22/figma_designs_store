209a28638d870a00f460b043e070b26d00d5d869
Abdelkader <zaroui.abedlkader777@gmail.com>
2026-01-18 14:27:16 +0100

    create models and migrate

--- core/models.py
+++ core/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Project(models.Model) :
+    name = models.CharField(max_length=255)
+    descriptions = models.TextField()
+    prototype_url = models.TextField()
+    price = models.DecimalField(max_digits=6,decimal_places=2)
+
+class ProjectImage(models.Model) :
+    image = models.ImageField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    project = models.ForeignKey(Project , on_delete=models.CASCADE, related_name='images')
+
+    def __str__(self):
+        return self.created_at
