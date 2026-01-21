72135f49c7040b464f71e131483aee9a591a53ba
Abdelkader <zaroui.abedlkader777@gmail.com>
2026-01-21 20:58:28 +0100

    create && migrate wishlist model

--- core/models.py
+++ core/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 
 class Project(models.Model) :
     name = models.CharField(max_length=255)
@@ -15,3 +15,11 @@ class ProjectImage(models.Model) :
 
     def __str__(self):
         return self.created_at
+
+class Wishlist(models.Model) :
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name="wishlist")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="wishlist")
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) :
+        return f"{self.project.name} wishlisted by {self.user.username}"
