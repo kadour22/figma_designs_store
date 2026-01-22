331bf08a891a725af119dfe1658ea1445cb3dfeb
Abdelkader <zaroui.abedlkader777@gmail.com>
2026-01-22 21:11:28 +0100

    create wishlist service GET

--- core/views.py
+++ core/views.py
@@ -55,4 +55,5 @@ class wishlist_service(APIView) :
             if serializer.is_valid() :
                 serializer.save(user = request.user)
                 return Response(serializer.data, status = status.HTTP_201_CREATED)
+            
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
